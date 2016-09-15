13dc6f2b325b07347b76eef47cdc9125c2994abf
kelly briceno <kellygavin96@gmail.com>
2016-09-15 18:16:27 -0400

    put in comments for what each method should do

--- item.java
+++ item.java
@@ -1,24 +1,27 @@
+//created public class item
 public class item {
+ //created properties for class
   String name;
   int weight;
   
+  //method returns string name
   public String getName() {
     return name; 
   }
-  
-  public void setName(int [] name) {
+  //method sets string name
+  public void setName(String [] name) {
     this.name = name;
   }
-  
+  //method returns integer weight
   public int getWeight() {
     return weight;
   }
-  
+  //method sets integer weight
   public void setWeight(int[] weight) {
     this.weight = weight;
   }
-  
+  //method to print a description of object and name and weight
   public String examine() {
-    System.out.println(name + weight); 
+    System.out.println("description of object" + name + weight); 
   }
 }
