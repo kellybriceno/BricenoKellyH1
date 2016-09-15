3fd90a539af6bd8bfbe7740bedfbe200d930deb6
kelly briceno <kellygavin96@gmail.com>
2016-09-15 18:13:28 -0400

    created weapon class and created weapon properties and methods

---
+++ weapon.java
@@ -0,0 +1,34 @@
+public class weapon {
+  int damage;
+  String name;
+  int weight;
+  
+  public int getDamage() {
+    return int damage;
+  }
+  
+  public void setDamage(int[] damage) {
+    this.setDamage = damage;
+  }
+  
+  public void setName(String[]name) {
+    this.setName = name;
+  }
+  
+  public getName() {
+    return String name;
+  }
+  
+  
+  public getWeight() {
+    return int weight;
+  }
+  
+  public void setWeight(Int[] weight) {
+    this.setWeight = weight;
+  }
+  
+  
+  public examine() {
+    System.out.println(name + weight);
+  }
