0e3721a24cef739e9e7f26a58bb49ca056306f75
kelly briceno <kellygavin96@gmail.com>
2016-09-16 13:06:08 -0400

    replaced void with int since it is returning something

--- item.java
+++ item.java
@@ -9,16 +9,16 @@ public class item {
     return name; 
   }
   //method sets string name
-  public void setName(String [] name) {
-    name = new name;
+  public String setName(String [] name) {
+    name = new name();
   }
   //method returns integer weight
   public int getWeight() {
     return weight;
   }
   //method sets integer weight
-  public void setWeight(int[] weight) {
-   weight = new weight;
+  public int setWeight(int[] weight) {
+   weight = new weight();
   }
   //method to print a description of object and name and weight
   public String examine() {
