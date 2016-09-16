2b73cab2f86390c0657796048be6f3bf54da6902
kelly briceno <kellygavin96@gmail.com>
2016-09-16 12:20:46 -0400

    set methods and properties for food.java class

--- food.java
+++ food.java
@@ -1,2 +1,31 @@
-public class food {
+public class food extends item {
+  // set properties
+  int nutrition;
+  int quantity;
+  
+  
+// getter method to get integer nutrition
+  public int getNutrition() {
+    return int nutrition;
 }
+  //setter for integer nutrition
+  public void setNutrition() {
+    nutrition = new nutrition; 
+  }
+  
+  //getter method to get integer quantity
+  public int getQuantity() {
+    return int quantity;
+  }
+  
+  //setter for integer quantity
+  public void setQuantity(int newQuantity) {
+    quantity = newQuantity; 
+ 
+  } 
+  getWeight(); 
+  getName();
+  examine(); 
+}
+  
+  
